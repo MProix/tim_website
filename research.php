8d43a50a7ad079c468a1ba4e8644d894a1cdb296
timpx <timpx@users.noreply.github.com>
2020-10-22 12:22:23 +0200

    update research

--- research.php
+++ research.php
@@ -12,6 +12,20 @@
         <?php include 'header.html'; ?>
 
         <div id="index_main_content">
+            <h3> Neural dynamics of overt and imagined speech.</h3>
+
+            <p>The human speech production system produce meaningful sentences through a large network of cortical regions 
+                that stream and encode information at multiple time scales. We use intracranial recordings and 
+                dynamical systems theory to model how different speech features are encoded and interact together 
+                in speech-related cortical areas.</p>
+
+
+            <h3> Seizure dynamics and forecasting.</h3>
+
+            <p>Neural activity of people with epilepsy is typically organized on several temporal and spatial scales. 
+                Using tools borrowed from dynamical systems theory and machine/statistical learning, 
+                we try to understand and model those dynamics. Our goal is to leverage those findings 
+                to offer effective diagnosis and forecasting tools for people with epilepsy.</p>
 
         </div>
 
